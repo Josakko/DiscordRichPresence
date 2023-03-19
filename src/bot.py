faf97a84b83acb2b2138c028a96aede70160a2d8
Josakko <112590914+Josakko@users.noreply.github.com>
2023-03-19 21:43:00 +0100

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,24 @@
 from pypresence import Presence
 import time
 import json
+import os
+from termcolor import colored
 
+config_file_path = 'config.json'
+config = {
+    "client_id": "None",
+    "state": "None",
+    "icon": "None",
+    "button_lbl": "None",
+    "button_url": "None"
+}
+
+if os.path.isfile(config_file_path):
+    with open(config_file_path, 'r') as config_file:
+        config_data = json.load(config_file)
+else:
+    with open(config_file_path, 'w') as config_file:
+        json.dump(config, config_file)
 
 with open("config.json") as f:
     config = json.load(f)
@@ -9,13 +26,13 @@ with open("config.json") as f:
 Id = config.get("client_id", None)
 
 if Id == "None":
-    clientId = input("> Enter ID of your App:")
+    clientId = input("> Enter ID of your App: ")
     with open("config.json", "w") as f:
         config["client_id"] = clientId
         json.dump(config, f, indent=2)
 else:
     clientId = config.get("client_id", None)
-    print("[+] Id from last run loaded!")
+    print(colored("[+] Id from last run loaded!", "green"))
 
 RPC = Presence(clientId)
 
@@ -23,51 +40,48 @@ RPC.connect()
 
 state = config.get("state", None)
 if state == "None":
-    state = input("> Enter message to be displayed:")
+    state = input("> Enter message to be displayed: ")
     with open("config.json", "w") as f:
         config["state"] = state
         json.dump(config, f, indent=2)
 else:
     state = config.get("state", None)
-    print("[+] Message loaded from last run!")
+    print(colored("[+] Message loaded from last run!", "green"))
 
 icon = config.get("icon", None)
 if icon == "None":
-    icon = input("> Enter image (icon) to be displayed:")
+    icon = input("> Enter image (icon) to be displayed: ")
     with open("config.json", "w") as f:
         config["icon"] = icon
         json.dump(config, f, indent=2)
 else:
     icon = config.get("icon", None)
-    print("[+] Icon loaded from last run!")
+    print(colored("[+] Icon loaded from last run!", "green"))
 
 button_lbl = config.get("button_lbl", None)
 if button_lbl == "None":
-    button_lbl = input("> Enter button text to be displayed:")
+    button_lbl = input("> Enter button text to be displayed: ")
     with open("config.json", "w") as f:
         config["button_lbl"] = button_lbl
         json.dump(config, f, indent=2)
 else:
     button_lbl = config.get("button_lbl", None)
-    print("[+] Button text loaded from last run!")
+    print(colored("[+] Button text loaded from last run!", "green"))
 
 button_url = config.get("button_url", None)
 if button_url == "None":
-    button_url = input("> Enter button URL to be used:")
+    button_url = input("> Enter button URL to be used: ")
     with open("config.json", "w") as f:
         config["button_url"] = button_url
         json.dump(config, f, indent=2)
 else:
     button_url = config.get("button_url", None)
-    print("[+] Button URL loaded from last run!")
+    print(colored("[+] Button URL loaded from last run!", "green"))
 
-RPC.update(
-    state = state,
-    large_image = icon,
-    buttons=[{"label": button_lbl, "url": button_url}]
-)
-print("[+] RPC updated")
+RPC.update(state = state, large_image = icon, buttons=[{"label": button_lbl, "url": button_url}])
+print(colored("[+] RPC updated", "blue"))
 
 while True:
     time.sleep(60)
+    print(colored("[+] RPC refreshed", "red"))
     
